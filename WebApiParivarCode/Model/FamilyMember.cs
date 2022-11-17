aa520400cf13336c47516ef25e78c52ec38a33b2
Ketan <senganiketan@gmail.com>
2022-11-17 11:48:11 +0530

    Added new columns

--- WebApiParivarCode/Model/FamilyMember.cs
+++ WebApiParivarCode/Model/FamilyMember.cs
@@ -6,19 +6,20 @@ namespace WebApiParivarCode.Model
     public class FamilyMember
     {
         public int FamilyMemberID { get; set; }
+        public int FamilyID { get; set; }
         public string? FirstName { get; set; }
         public string? FatherHusbandName { get; set; }
+        public int RelationID { get; set; }
         public string? Gender { get; set; }
         public DateTime Birthdate { get; set; }
         public string? MaritalStatus { get; set; }
         public string? Education { get; set; }
         public string? Business { get; set; }
         public int Mobile { get; set; }
+        public bool AttendingProgram { get; set; }
+        public bool Active { get; set; }
         public int ModifiedByID { get; set; }
         public DateTime ModifiedDate { get; set; }
-        public bool Active { get; set; }
-        public int FamilyID { get; set; }
-        public int RelationID { get; set; }
         public string? RelationName { get; set; }
     }
 }
