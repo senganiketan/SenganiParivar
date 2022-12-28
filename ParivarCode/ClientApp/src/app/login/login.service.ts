6551b1387148f1b0a05d98a0270a842bd9c7d764
Ketan <ketan.patel@prounlimited.com>
2022-12-28 12:45:48 +0530

    Added Login Service

---
+++ ParivarCode/ClientApp/src/app/login/login.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class LoginService {
+
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  
+}
