81c969aaccfee549d2d2b2f5ac2a7637f8bac96a
Ketan <ketan.patel@prounlimited.com>
2023-01-11 21:38:53 +0530

    Done Production settings

--- WebApiParivarCode/Program.cs
+++ WebApiParivarCode/Program.cs
@@ -6,15 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr"))
 );
 
-builder.Services.AddCors(options => options.AddPolicy(name: "FamilyOrigins",
-    policy =>
-    {
-        policy.WithOrigins("http://localhost:44489", "https://localhost:44489").AllowAnyMethod().AllowAnyHeader();
-    }));
+builder.Services.AddCors(options => options.AddPolicy(name: "FamilyOrigins", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+//builder.Services.AddCors(options => options.AddPolicy(name: "FamilyOrigins",
+//    policy =>
+//    {
+//       // policy.WithOrigins("http://webapi.hirjidada.com", "https://webapi.hirjidada.com").AllowAnyMethod().AllowAnyHeader();
+//        policy.WithOrigins("http://webapi.hirjidada.com", "https://webapi.hirjidada.com").AllowAnyMethod().AllowAnyHeader();
+//    }));
 
 
 builder.Services.AddScoped<IFamilyRepository, FamilyRepository>();
@@ -32,11 +35,15 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+
 app.UseCors("FamilyOrigins");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.Run();
+
+
