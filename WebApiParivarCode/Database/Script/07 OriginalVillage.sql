545414c30127c2d949caa78c248bffcbc0422f33
Ketan <ketan.patel@prounlimited.com>
2023-02-15 15:58:49 +0530

    sorting original village table data

--- WebApiParivarCode/Database/Script/07 OriginalVillage.sql
+++ WebApiParivarCode/Database/Script/07 OriginalVillage.sql
@@ -15,32 +15,30 @@ GO
 GO
 
 
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રાયણ','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ગઢસીસા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'અરજણપુરા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
 Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'કોટડા(ચ)','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
 Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ખોભડી','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રામપર(સ)','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ગઢસીસા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
 Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ઘડુલી','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ધારેશી','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ટોડીયા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'તલવાણા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'દુર્ગાપર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'દેશલપર(વાંઢાય)','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
 Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'દોલતપર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ધારેશી','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'પદમપુર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'બિદડા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ભીટારા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'મદનપુરા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'મમાયમોરા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'મેરાઉ','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રત્નાપર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રસલીયા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રાજપર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રામપર(સ)','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રાયણ','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
+Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'લુડવા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
 Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'વાલકા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
 Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'વિરાણી કોરાવાળી','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'દુર્ગાપર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'પદમપુર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રાજપર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ભીટારા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'લુડવા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રત્નાપર','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'દેશલપર(વાંઢાય)','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'બિદડા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'તલવાણા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'મેરાઉ','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'મદનપુરા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'ટોડીયા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'રસલીયા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'અરજણપુરા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'મમાયમોરા','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
 Insert INTO OriginalVillage (OriginalVillageName,Person1Name,Person1Mobile,Person2Name,Person2Mobile) values(N'અન્ય','Ketan Hiralal',9979253044,'Dilip Hiralal',9825975949);
-
-
 
